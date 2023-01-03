30e8726755c42dfdf885b4ee41fdc099ab2942a9
GastonYasuda <gastonyasuda@gmail.com>
2023-01-03 20:50:38 -0300

    clases

--- Clase 6 - Promises and async/clase6/src/components/ItemList/index.jsx
+++ Clase 6 - Promises and async/clase6/src/components/ItemList/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Item from '../Item'
 
-const ItemList = ({productos}) => {
+const ItemList = ({ productos }) => {
   return (
     <div>
-        {productos.map(producto => {
-            return <Item product={producto} key={producto.id}/>
-        })}
+      {productos.map(producto => {
+        return <Item product={producto} key={producto.id} />
+      })}
     </div>
   )
 }
